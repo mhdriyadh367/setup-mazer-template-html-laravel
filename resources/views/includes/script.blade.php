8bdb5babbb8d14326e35c40a0d4efe0c4d03ffae
mhdriyadh <user@users-MacBook-Pro.local>
2023-04-15 02:48:21 +0700

    first commit

---
+++ resources/views/includes/script.blade.php
@@ -0,0 +1,6 @@
+<script src="{{ asset('dist/assets/js/bootstrap.js') }}"></script>
+<script src="{{ asset('dist/assets/js/app.js') }}"></script>
+
+<!-- Need: Apexcharts -->
+<script src="{{ asset('dist/assets/extensions/apexcharts/apexcharts.min.js') }}"></script>
+<script src="{{ asset('dist/assets/js/pages/dashboard.js') }}"></script>
